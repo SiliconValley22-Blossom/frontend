a11a916ea76f04755244cc4b48527c5a6cfd0460
Youngjin <yjshin229@gmail.com>
2022-07-14 18:25:46 +0900

    Docs: add Hamburger icon

---
+++ src/components/atom/Hamburger.jsx
@@ -0,0 +1,11 @@
+import {TiThMenuOutline} from "react-icons/ti"; 
+
+function Hamburger(){
+    return(
+        <div>
+            <TiThMenuOutline className="hamburger"/>
+        </div>
+    )
+}
+export default Hamburger;
+ 
