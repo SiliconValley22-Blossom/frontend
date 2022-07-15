fb695602d1b44ec5d2fb31a936efb004e2be796d
Youngjin <yjshin229@gmail.com>
2022-07-15 18:12:26 +0900

    fix: responsive web design implemented

--- src/components/molecule/NavBar.jsx
+++ src/components/molecule/NavBar.jsx
@@ -1,15 +1,33 @@
-import Hamburger from "../atom/Hamburger";
 import React from 'react';
+import styled from 'styled-components';
+import {ThMenuOutline} from '@styled-icons/typicons/ThMenuOutline';
+
+
+
+const StyledHeader = styled.div`
+    display: flex;
+    background: var(--main-color);
+    width: 1;
+    flex-direction: row;
+    flex : 1;
+    color: white;
+    padding: 1rem;
+    font-weight: 600;
+    font-size: 2rem;
+    justify-content: space-between;
+
+    .ham{
+        width: 2rem;
+        height: 2rem;
+    }
+`;
 
 function NavBar(){
     return(
-        <div className = "NavBar">
-            <div className = "pink-nav">
-                <div>Blossom</div>
-                <Hamburger></Hamburger>
-            </div>
-        </div>
+        <StyledHeader>
+            <div>Blossom</div>
+            <ThMenuOutline className='ham'></ThMenuOutline>
+        </StyledHeader>
     )
 }
-
 export default NavBar;
