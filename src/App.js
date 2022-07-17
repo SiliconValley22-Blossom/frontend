b5b09c26c8bdeeaeb40de0fd099602690427d5f1
Youngjin <yjshin229@gmail.com>
2022-07-17 18:30:00 +0900

    fix: App.js import path fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Modal from './organisms/Modal';
-import NavBar from './molecule/NavBar';
+import Modal from './components/organisms/Modal';
+import NavBar from './components/molecule/NavBar';
 import './App.css';
 import React from 'react';
 
