9880746c4026465706f33274bca6e1f82bc74634
Youngjin <yjshin229@gmail.com>
2022-07-17 18:46:01 +0900

    Docs: Add new modal page

---
+++ src/components/organisms/Modal-login.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Button from "../atom/Button";
+import TextLink from "../atom/TextLink";
+import Input from "../atom/Input";
+import styled from 'styled-components';
+
+const StyledModal = styled.div`
+  margin: 4.5rem auto;
+  width: 50%;
+  background: var(--main-bg-color);
+  border-radius: 2rem;
+`;
+
+function Modal(){
+    return(
+      <StyledModal>
+        <p><Input></Input></p>
+        <p ><Input ></Input></p>
+        <p><Button>로그인</Button></p>
+        <TextLink>회원가입</TextLink>
+        <TextLink>ID/PW찾기</TextLink>
+        </StyledModal>
+    )
+  }
+
+  export default Modal;
