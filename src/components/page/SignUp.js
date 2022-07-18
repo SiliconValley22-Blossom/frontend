8c68a6184712ffc009d4b50d84c8b9398c69ec38
Youngjin <yjshin229@gmail.com>
2022-07-19 04:47:24 +0900

    Style: className modifed

---
+++ src/components/page/SignUp.js
@@ -0,0 +1,14 @@
+import ModalSignUp from '../organisms/ModalSignUp';
+import '../../App.css';
+import React from 'react';
+
+
+function SignUp(){
+    return(
+        <div className='signup'>
+            <ModalSignUp/>
+        </div>
+        
+    )
+}
+export default SignUp;
