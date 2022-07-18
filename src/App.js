9e01ecdabe403fda201017de9ffdd32880f0b160
Youngjin <yjshin229@gmail.com>
2022-07-19 04:45:44 +0900

    Feat: drop down menu, routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import ModalSignUp from './components/organisms/Modal-signup';
-import NavBar from './components/molecule/NavBar';
-import './App.css';
 import React from 'react';
+import NavBar from './components/molecule/NavBar/NavBar';
+import Home from './components/page/Home'
+import MyPage from './components/page/MyPage'
+import Login from './components/page/Login'
+import SignUp from './components/page/SignUp'
+import PhotoChange from './components/page/PhotoChange'
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-//this shows the modal-signup page
-//needs routing 
+
+
 function App() {
   return (
-    <div className="App">
-      <NavBar></NavBar>
-      <ModalSignUp/>
+    <div>
+      <BrowserRouter>
+        <NavBar/>
+        <Switch>
+          <Route path = "/" exact component = {Home}/>
+          <Route path = "/Login" component = {Login}/>
+          <Route path = "/SignUp" component = {SignUp}/>
+          <Route path = "/PhotoChange" component = {PhotoChange}/>
+          <Route path = "/MyPage" component = {MyPage}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
