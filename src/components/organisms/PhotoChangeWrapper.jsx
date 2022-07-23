89e9f0c23d8b01c6eaafce6b91396aa0279b569b
Jiyoon Kim <rlawldbs0911@naver.com>
2022-07-23 11:52:27 +0900

    feat: upload image 미리보기

--- src/components/organisms/PhotoChangeWrapper.jsx
+++ src/components/organisms/PhotoChangeWrapper.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import '../../App.css'
+import React, {useState} from 'react';
 import Button from "../atom/Button";
 import DownloadButton from '../atom/DownloadButton';
 import Display from '../atom/Display';
@@ -10,37 +9,45 @@ const StyledPhotoChange = styled.div`
   margin: 4.5rem auto;
 `;
 
-function postImage(){
-    const data = new FormData();
-    data.append('file',this.uploadInput.files[0])
-    data.append('filename',this.fileName.vaule);
-    axios({
-        method: "post",
-        url: "/image",
-        data: data
-    }).then((res) => {})
-}
+// function postImage(){
+//     const data = new FormData();
+//     data.append('file',this.uploadInput.files[0])
+//     data.append('filename',this.fileName.vaule);
+//     axios({
+//         method: "post",
+//         url: "/image",
+//         data: data
+//     }).then((res) => {})
+// }
 
 function PhotoChange(){
+
+    const [fileImage, setFileImage] = useState("");
+    const saveFileImage = (event: React.ChangeEvent<HTMLInputElement>) =>{
+        setFileImage(URL.createObjectURL(event.target.files[0]));
+    };
+
+    const imageStyle={
+        display:"block",
+        width:"25rem",
+        height:"auto",
+        border:"1px solid black"
+    }
+
     return(
-            <StyledPhotoChange>
-                <p>
-                    <div style = {{display : 'flex'}}>
-                    
-                        {<Display></Display>}
-                        {<Display></Display>}
-                    </div>
-                </p>
-                <p>
-                    <div className="upload">
-                        <input type="file"/>
-                        <Button onClick={postImage()}>업로드</Button>
-                        
-                    </div>
-                        <DownloadButton/>
-                </p>
-                
-            </StyledPhotoChange>
+        <StyledPhotoChange>
+            <div style={{alignItems: "center", justifyContent: "center", }}>
+            
+            </div>
+            <div style={imageStyle}>{fileImage && ( <img alt="sample" src={fileImage}
+                                        style={{ margin: "auto" }} /> )}
+            </div>
+            <input
+                name="imggeUpload"
+                type="file"
+                accept="image/*"
+                onChange={saveFileImage} />
+        </StyledPhotoChange>
     )
 }
 
