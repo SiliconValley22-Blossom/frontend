cc7a98dc63fed53b867c8191ae0859becf4a125b
Youngjin <yjshin229@gmail.com>
2022-07-26 03:06:43 +0900

    Fix: path

--- src/components/page/Home.jsx
+++ src/components/page/Home.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import '../../App.css'
-import { Link, Route, BrowserRouter as Router } from "react-router-dom";
 import HomeWrapper from '../organisms/HomeWrapper';
 
 
