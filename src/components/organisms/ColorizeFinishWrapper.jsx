bdfb0e5153047cc49d672d3f1d195d108e00770e
Youngjin <yjshin229@gmail.com>
2022-07-26 14:11:49 +0900

    Feat: added route to Colorize Finish

---
+++ src/components/organisms/ColorizeFinishWrapper.jsx
@@ -0,0 +1,67 @@
+import React, { useState} from "react";
+import styled from "styled-components";
+import Button from "../atom/Button";
+import NavBar from "../molecule/NavBar/NavBar_colorize";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+display:flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`;
+
+const StyledDropDown = styled.div`
+  width: 30rem;
+  height: 40rem;
+  background-color: white;
+  border-radius: 50px;
+  margin: 3rem auto 1.5rem;
+  display: flex;
+`;
+
+const ColFinButton = styled.button`
+  padding: 0.5rem 0.5rem;
+  border-radius: 1rem;
+  border-style: solid;
+  border-color: var(--sub-purple);
+  color: gray;
+  text-align: center;
+  background: var(--main-pink);
+  width: 11rem;
+  margin: 1rem 1rem 0rem 1rem;
+  cursor: pointer;
+  font-size: 1.3rem;
+  font-weight: bold;
+
+  &:hover {
+    color: white;
+    background-color: transparent;
+    border-style: solid;
+    border-color: var(--dark-gray);
+    transition : all 0.2s ease-out;
+  }
+`;
+
+
+
+function ColorizeFinishWrapper() {
+  const [file, setFile] = useState("");
+
+  return (
+    <>
+      <NavBar />
+
+      <StyledDropDown>
+        <img style={{width: '5rem' ,height:'5rem', margin: 'auto auto'}} src= "img/FlowerLogo.png"/>
+      </StyledDropDown>
+      <ColFinButton>Download</ColFinButton>
+     <ColFinButton>
+       <Link to="/Colorize"></Link>
+       Go Colorize!
+     </ColFinButton>
+    </>
+  );
+};
+
+export default ColorizeFinishWrapper;
