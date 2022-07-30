491806812bad7414df034d2fe69944d77c6c1775
Youngjin <yjshin229@gmail.com>
2022-07-31 03:30:32 +0900

    Fix: Navbar colorize implemented
    Style identical with navbar_mypage

--- src/components/molecule/NavBar/NavBar_colorize.jsx
+++ src/components/molecule/NavBar/NavBar_colorize.jsx
@@ -5,15 +5,33 @@ import styled from 'styled-components';
 import './NavBar_colorize.css';
 import { Cookies } from 'react-cookie';
 import {PersonFill} from '@styled-icons/bootstrap/PersonFill';
+import {DownArrow} from '@styled-icons/boxicons-solid/DownArrow';
 
 const StyledHeader = styled.nav`
     height: 3rem;
     width:100%;
-    display: inline-block;
+    display: flex;
+    align-items: center;
     border-style: solid none;
     border-color: transparent transparent white;
     font-size: 1.5rem;
     font-weight : bold;
+
+    @media screen and (max-width:700px) {
+        justify-content: center;
+    }
+`;
+
+const StyledArrow = styled(DownArrow)`
+    display: none;
+
+    @media screen and (max-width:700px) {
+        height: 1.3rem;
+        cursor: pointer;
+        display:flex;
+        color: var(--sub-purple);
+        margin-left: 0.5rem;
+}
 `;
 
 const NavBar = () => {
@@ -34,6 +52,7 @@ const NavBar = () => {
             <Link to ='/' className='navbar-logo-colorize'>
             <img style={{width: '11rem'}} alt="blossomLogo" src="../../../../logo-4.png"/>
             </Link>
+            <StyledArrow></StyledArrow>
     
             <ul className = 'nav-menu-colorize'>
 
